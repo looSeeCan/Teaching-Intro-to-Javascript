3440dd295394bd1555769eea9a589a3857f84038
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2023-02-04 22:05:21 -0600

    Saturday Night
    finished with loops, gotta finsh the rest before class on monday

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-document.title = "button";
+// document.title = "button";
 
-let person = {
-    firstName: "John", ///key: value
-    lastName: "William",
-    age: 18 ,
-    gender: "female" ,
-    ethnicity: "Martian",
-    job: "Therapist",
+// let person = {
+//     firstName: "John", ///key: value
+//     lastName: "William",
+//     age: 18 ,
+//     gender: "female" ,
+//     ethnicity: "Martian",
+//     job: "Therapist",
     
-    greeting: function() {
-        console.log(
-            `Hello World, My name is ${this.firstName} and I am ${this.age}. I am the ${this.job}`
-            )
-        }
-    };
+//     greeting: function() {
+//         console.log(
+//             `Hello World, My name is ${this.firstName} and I am ${this.age}. I am the ${this.job}`
+//             )
+//         }
+//     };
     
-    console.log(person.firstName.toLocaleLowerCase())
-    // person.greeting();
+//     console.log(person.firstName.toLocaleLowerCase())
+//     // person.greeting();
 
-    let btn = document.getElementById('btn');
-    console.log(btn);
-    btn.addEventListener("click", function() {
-        console.log("test");
-    });
+//     let btn = document.getElementById('btn');
+//     console.log(btn);
+//     btn.addEventListener("click", function() {
+//         console.log("test");
+//     });
