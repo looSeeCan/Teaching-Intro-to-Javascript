c5f12e9d019266d84e225da6ab79371101808c27
looSeeCan2 <nruabnrabkojthiabkuv@gmail.com>
2023-02-14 19:36:49 -0600

    done with functions day
    did a zoom class on this. check zoom recording

--- functions/functions.js
+++ functions/functions.js
@@ -1,27 +1,27 @@
-///1. const stringVariable = "Hello World";//keyword (const), identifier(string), assignment operator(=), value(Hello World), semicolon(they are read and used y the compiler to distinguish between seperate statements)
-const stringVariable = "Hello World";
-console.log(stringVariable);
-/// const stringNameVariable = "Lucycan"
-const stringNameVariable = "Lucycan";
-console.log(stringNameVariable);
-///2. const booleanVariable = true;
-const booleanVariable = true;
-console.log(booleanVariable);
-///3. const null1 = null;
-const nullVariable = null;
-console.log(nullVariable);
-///4. let undefinedVariable1;
-let undefinedVariable;
-console.log(undefinedVariable);
-///5. const num = 44;
-const num = 44;
-console.log(num);
-///6. const objVariable = {firstName: "Lucycan", lastName: "Ly"};
-const objVariable = {
-    firstName: "Lucycan",
-    lastName: "Ly"
-};
-console.log(objVariable);
+// ///1. const stringVariable = "Hello World";//keyword (const), identifier(string), assignment operator(=), value(Hello World), semicolon(they are read and used y the compiler to distinguish between seperate statements)
+// const stringVariable = "Hello World";
+// console.log(stringVariable);
+// /// const stringNameVariable = "Lucycan"
+// const stringNameVariable = "Lucycan";
+// console.log(stringNameVariable);
+// ///2. const booleanVariable = true;
+// const booleanVariable = true;
+// console.log(booleanVariable);
+// ///3. const null1 = null;
+// const nullVariable = null;
+// console.log(nullVariable);
+// ///4. let undefinedVariable1;
+// let undefinedVariable;
+// console.log(undefinedVariable);
+// ///5. const num = 44;
+// const num = 44;
+// console.log(num);
+// ///6. const objVariable = {firstName: "Lucycan", lastName: "Ly"};
+// const objVariable = {
+//     firstName: "Lucycan",
+//     lastName: "Ly"
+// };
+// console.log(objVariable);
 ///////////////////////////////////////////
 
 /// function greeting return Hello World My name is Lucycan /// regular function, arrow/ scope
@@ -29,6 +29,14 @@ console.log(objVariable);
 // function emptyFunction() {
 //     return "This is an empty function";
 // };
+
+function emptyFunction() {
+    return "this is an Empty Function"
+};
+
+const x = emptyFunction();
+console.log(x);
+
 
 // emptyFunction(); /// calling functions
 // console.log(emptyFunction());
@@ -40,6 +48,14 @@ console.log(objVariable);
 // };
 // const returnedValue = myFunction(); /// assign it to a variable
 // console.log(returnedValue);
+function myFunction() {
+    const myArray = ["Hello World", "Lucycan", null,  undefined, 44, {firstName: "Lucycan", lastName: "Ly"}]
+    return myArray;
+};
+
+const y = myFunction();
+console.log(y);
+
 
 // const myArrowFunction = () => { 
 //     const myArray = ["Hello World", "Lucycan", null, undefined, 44, {firstname: "Lucycan", lastName: "Ly"}];
@@ -47,6 +63,18 @@ console.log(objVariable);
 // };
 // const y = myArrowFunction();
 // console.log(y);
+
+const myArrowFunction = () => {
+    const myArray = ["Hello World", "Lucycan", null,  undefined, 44, {firstName: "Lucycan", lastName: "Ly"}]
+    return myArray;
+
+};
+const z = myArrowFunction();
+console.log(z);
+console.log(z[1]);
+
+console.log(`${z[0]} my name is ${z[1]}`);
+
 
 // /// parameters and argutments
 // const myArrowFunction1 = (parameter) => {
@@ -58,6 +86,16 @@ console.log(objVariable);
     
 //     return myArray;
 // };
+
+const myArrowFunction1 = (parameter) => {
+    return parameter;
+};
+
+const a = myArrowFunction1();
+console.log(a);
+
+const b = myArrowFunction1("this argument could be anything");
+console.log(b)
 
 // const z = myArrowFunction1();
 // console.log(z);
@@ -72,26 +110,33 @@ console.log(objVariable);
 
 /// grab the element and assign it to a variable named aboutMe
 
+    const aboutMeFunction = (name, age, picture) => {
 
-    // const aboutMe = document.getElementById("aboutMe");
-    // /// parageraph
-    // const paragraph = document.querySelector(".paragraph");
-    // console.log(paragraph); /// show the empty tag which is falsy
-    
-    // aboutMe.addEventListener('click', function() { 
-    //     const stringVariable = "Hello World";
-    //     const stringNameVariable = "Lucycan";
-    //     const num = 44;
-    //     const imageSource = "../assets/longHairDontCare.jpg"; 
+        const aboutMe = document.getElementById("aboutMe");
+        /// parageraph
+        const paragraph = document.querySelector(".paragraph");
+        console.log(paragraph); /// show the empty tag which is falsy
         
-    //     if(paragraph.innerText) {
-    //         console.log("The paragraph already has text");
-    //     }else {
-    //         console.log(false);
-    //         paragraph.innerText = `${stringVariable} my name is ${stringNameVariable} and I am ${num}`;
-    //     }
+        aboutMe.addEventListener('click', function() { 
+            const stringVariable = "Hello World";
+            const stringNameVariable = name;
+            const num = age;
+            const imageSource = picture; 
+            
+            if(paragraph.innerText) {
+                console.log("The paragraph already has text");
+            }else {
+                console.log(false);
+                paragraph.innerText = `${stringVariable} my name is ${stringNameVariable} and I am ${num}`;
+            }
+    
+            const img = document.querySelector(".img");
+            img.src = imageSource;
+        })
+    };
 
-    //     const img = document.querySelector(".img");
-    //     img.src = imageSource;
-    // })
+    const c = aboutMeFunction("ChunLi", 23, "../assets/chunli.jpg" );
+    const d = aboutMeFunction("Lucycan", 44, "../assets/longHairDontCare.jpg")
 
+    const myFunctionArray = [c, d];
+    console.log(myFunctionArray);
